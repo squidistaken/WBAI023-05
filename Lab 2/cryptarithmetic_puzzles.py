3642113d0026edefc19a9383f86a67abe974bc02
Marcus Persson <marcusper2003@gmail.com>
2024-05-19 13:52:02 +0200

    constraint fixes
    not done!!!

--- Lab 2/cryptarithmetic_puzzles.py
+++ Lab 2/cryptarithmetic_puzzles.py
@@ -2,35 +2,37 @@ from CSP_solver import *
 from utils import all_diff
 
 # Modify here
-puzzle = "ONE + NINE + TWENTY + FIFTY = EIGHTY"
+puzzle = "SEND + MORE = MONEY"
 
 letters = []
 
+# Variables
 for ch in puzzle:
-    if str.isalpha(ch) and ch not in letters:
+    if ch.isalpha() and ch not in letters:
         letters.append(ch)
-
-# TODO: Do we even need this?
-formatted_puzzle = ""
-for ch in puzzle:
-    if ch.isalpha():
-        formatted_puzzle += "{" + ch + "}"
-    elif ch == "=":
-        formatted_puzzle += "=="
-    else:
-        formatted_puzzle += ch
 
 variables = []
 
 for l in letters:
-    v = Variable(l, domain=list(range(len(letters))))
+    v = Variable(l, domain=list(range(0, 10)))
     variables.append(v)
 
+# Constraints
+# Each letter stands for a unique digit, and the same letter stands for the same digit throughout the puzzle.
 constraints = all_diff(variables)
 
-# TODO: Fix constraint for puzzle
-c = Constraint(puzzle.replace("=", "=="))
-constraints.append(c)
+beginners = []
+for i in puzzle.split():
+    if i.isalpha():
+        # TODO: Fix this constraint
+        constraints.append(Constraint(f"{i} = \"\".join({list(i)})"))
+        if i[0] not in beginners:
+            beginners.append(i[0])
+            # There must be no leading zeros.
+            constraints.append(Constraint(f"{i[0]} != 0"))
+
+
+constraints.append(Constraint(puzzle.replace("=", "==")))
 
 csp = CSP(variables, constraints, keep_node=False, keep_arc=False, heuristic=None)
 csp.solve(verbose=False)
